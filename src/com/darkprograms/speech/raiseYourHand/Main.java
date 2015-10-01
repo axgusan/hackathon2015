99599da4e5b12925fd56b8b717f66a7e2e24a2f7
Aleksandr Gusan <Aleksandr.Gusan@Pearson.com>
2015-10-01 09:46:06 -0600

    fixed format

--- src/com/darkprograms/speech/raiseYourHand/Main.java
+++ src/com/darkprograms/speech/raiseYourHand/Main.java
@@ -8,40 +8,50 @@ import com.darkprograms.speech.recognizer.GSpeechDuplex;
 import com.darkprograms.speech.recognizer.GSpeechResponseListener;
 import com.darkprograms.speech.recognizer.GoogleResponse;
 
+public class Main {
 
-public class Main{
-	
-public static void main(String[] args){
-    GSpeechDuplex dup = new GSpeechDuplex(ConfigVariables.key);//Instantiate the API
-    
-    dup.addResponseListener(new GSpeechResponseListener(){// Adds the listener
-    	public void onResponse(GoogleResponse gr){
-            System.out.println("Google thinks you said: " + gr.getResponse());
-            System.out.println("with " + 
-            ((gr.getConfidence()!=null)?(Double.parseDouble(gr.getConfidence())*100):null) 
-                + "% confidence.");
-            System.out.println("Google also thinks that you might have said:" 
-                    + gr.getOtherPossibleResponses());
-        }
-    });
-    Microphone mic = new Microphone(FLACFileWriter.FLAC);//Instantiate microphone and have 
-    // it record FLAC file.
-    File file = new File("CRAudioTest.flac");//The File to record the buffer to. 
-    //You can also create your own buffer using the getTargetDataLine() method.
-    while(true){
-        try{
-            mic.captureAudioToFile(file);//Begins recording
-            Thread.sleep(10000);//Records for 10 seconds
-            mic.close();//Stops recording
-            //Sends 10 second voice recording to Google
-            byte[] data = Files.readAllBytes(mic.getAudioFile().toPath());//Saves data into memory.
-                    dup.recognize(data, (int)mic.getAudioFormat().getSampleRate());
-            mic.getAudioFile().delete();//Deletes Buffer file
-            //REPEAT
-        }
-        catch(Exception ex){
-            ex.printStackTrace();//Prints an error if something goes wrong.
-        }
-    }
-}
+	public static void main(String[] args) {
+		GSpeechDuplex dup = new GSpeechDuplex(ConfigVariables.key);// Instantiate
+																	// the API
+
+		dup.addResponseListener(new GSpeechResponseListener() {// Adds the
+																// listener
+			public void onResponse(GoogleResponse gr) {
+				System.out.println("Google thinks you said: "
+						+ gr.getResponse());
+				System.out.println("with "
+						+ ((gr.getConfidence() != null) ? (Double
+								.parseDouble(gr.getConfidence()) * 100) : null)
+						+ "% confidence.");
+				System.out
+						.println("Google also thinks that you might have said:"
+								+ gr.getOtherPossibleResponses());
+			}
+		});
+		Microphone mic = new Microphone(FLACFileWriter.FLAC);// Instantiate
+																// microphone
+																// and have
+		// it record FLAC file.
+		File file = new File("CRAudioTest.flac");// The File to record the
+													// buffer to.
+		// You can also create your own buffer using the getTargetDataLine()
+		// method.
+		while (true) {
+			try {
+				mic.captureAudioToFile(file);// Begins recording
+				Thread.sleep(10000);// Records for 10 seconds
+				mic.close();// Stops recording
+				// Sends 10 second voice recording to Google
+				byte[] data = Files.readAllBytes(mic.getAudioFile().toPath());// Saves
+																				// data
+																				// into
+																				// memory.
+				dup.recognize(data, (int) mic.getAudioFormat().getSampleRate());
+				mic.getAudioFile().delete();// Deletes Buffer file
+				// REPEAT
+			} catch (Exception ex) {
+				ex.printStackTrace();// Prints an error if something goes wrong.
+			}
+		}
+	}
 }

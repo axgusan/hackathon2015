59ed90718b09641904ffd80a566f7f22f923250e
Aleksandr Gusan <Aleksandr.Gusan@Pearson.com>
2015-10-02 10:26:56 -0600

    committed out references to missing images in NotePanel styles

--- src/com/pearson/hackathon/ui/NotePanel.java
+++ src/com/pearson/hackathon/ui/NotePanel.java
@@ -79,27 +79,27 @@ public class NotePanel extends JPanel {
 		s = doc.addStyle("large", regular);
 		StyleConstants.setFontSize(s, 16);
 
-		s = doc.addStyle("icon", regular);
-		StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER);
-		ImageIcon pigIcon = createImageIcon("images/Pig.gif", "a cute pig");
-		if (pigIcon != null) {
-			StyleConstants.setIcon(s, pigIcon);
-		}
-
-		s = doc.addStyle("button", regular);
-		StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER);
-		ImageIcon soundIcon = createImageIcon("images/sound.gif", "sound icon");
-		JButton button = new JButton();
-		if (soundIcon != null) {
-			button.setIcon(soundIcon);
-		} else {
-			button.setText("BEEP");
-		}
-		button.setCursor(Cursor.getDefaultCursor());
-		button.setMargin(new Insets(0, 0, 0, 0));
-		button.setActionCommand("placeholderButton");
-		// button.addActionListener(this);
-		StyleConstants.setComponent(s, button);
+//		s = doc.addStyle("icon", regular);
+//		StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER);
+//		ImageIcon pigIcon = createImageIcon("images/Pig.gif", "a cute pig");
+//		if (pigIcon != null) {
+//			StyleConstants.setIcon(s, pigIcon);
+//		}
+//
+//		s = doc.addStyle("button", regular);
+//		StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER);
+//		ImageIcon soundIcon = createImageIcon("images/sound.gif", "sound icon");
+//		JButton button = new JButton();
+//		if (soundIcon != null) {
+//			button.setIcon(soundIcon);
+//		} else {
+//			button.setText("BEEP");
+//		}
+//		button.setCursor(Cursor.getDefaultCursor());
+//		button.setMargin(new Insets(0, 0, 0, 0));
+//		button.setActionCommand("placeholderButton");
+//		// button.addActionListener(this);
+//		StyleConstants.setComponent(s, button);
 	}
 
 	/** Returns an ImageIcon, or null if the path was invalid. */
